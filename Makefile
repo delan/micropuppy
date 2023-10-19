b01476f30bebaa107fedb5ec959173b3217d9cab
Delan Azabani <delan@azabani.com>
2023-10-19 22:55:10 +0800

    use device tree to get pl011 memory region

--- Makefile
+++ Makefile
@@ -6,7 +6,10 @@ CARGO_PROFILE_FLAG =
 run-kernel: kernel
 	cd qemu && make run-kernel KERNEL=../kernel/target/aarch64-unknown-none/$(CARGO_PROFILE_DIR)/kernel
 
-kernel:
+kernel: qemu/virt-8.0.dtb
 	cd kernel && cargo build $(CARGO_PROFILE_FLAG)
+
+qemu/virt-8.0.dtb:
+	cd qemu && make virt-8.0.dtb
 
 .PHONY: kernel
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -14,11 +14,19 @@ fn on_panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn kernel_main() {
-    const UART0: *mut u8 = 0x0900_0000 as *mut u8;
+    // TODO not this
+    // https://qemu-project.gitlab.io/qemu/system/arm/virt.html#hardware-configuration-information-for-bare-metal-programming
+    // https://doc.coreboot.org/mainboard/emulation/qemu-aarch64.html
+    // https://mail.gnu.org/archive/html/qemu-discuss/2019-10/msg00014.html
+    let fdt = fdt::Fdt::new(include_bytes!("../../qemu/virt-8.0.dtb")).unwrap();
+    let uart0 = fdt.find_compatible(&["arm,pl011"]).unwrap();
+    let uart0 = uart0.reg().unwrap().next().unwrap();
+    let uart0_uartdr = uart0.starting_address as *mut u8;
+
     let out_str = b"woof!!!! wraaaooo!!";
     for byte in out_str {
         unsafe {
-            ptr::write_volatile(UART0, *byte);
+            ptr::write_volatile(uart0_uartdr, *byte);
         }
     }
 }
