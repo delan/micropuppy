319e9c2a9212408e70a9a3a2ab1b67f298ba610a
Delan Azabani <delan@azabani.com>
2023-10-20 17:09:49 +0800

    use ufmt for printing to uart0

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,8 +2,13 @@
 #![no_main]
 
 use core::arch::global_asm;
+use core::convert::Infallible;
 use core::panic::PanicInfo;
 use core::ptr;
+
+use fdt::standard_nodes::MemoryRegion;
+use ufmt::uwriteln;
+use ufmt_write::uWrite;
 
 global_asm!(include_str!("start.s"));
 
@@ -21,12 +26,28 @@ pub extern "C" fn kernel_main() {
     let fdt = fdt::Fdt::new(include_bytes!("../../qemu/virt-8.0.dtb")).unwrap();
     let uart0 = fdt.find_compatible(&["arm,pl011"]).unwrap();
     let uart0 = uart0.reg().unwrap().next().unwrap();
-    let uart0_uartdr = uart0.starting_address as *mut u8;
+    let mut uart0 = Pl011Writer::new(&uart0);
 
-    let out_str = b"woof!!!! wraaaooo!!";
-    for byte in out_str {
-        unsafe {
-            ptr::write_volatile(uart0_uartdr, *byte);
+    uwriteln!(uart0, "woof!!!! wraaaooo!!").unwrap();
+}
+
+struct Pl011Writer(*mut u8);
+
+impl Pl011Writer {
+    fn new(uart: &MemoryRegion) -> Self {
+        // UARTDR is at starting address
+        Self(uart.starting_address as *mut u8)
+    }
+}
+
+impl uWrite for Pl011Writer {
+    type Error = Infallible;
+
+    fn write_str(&mut self, value: &str) -> Result<(), Self::Error> {
+        for byte in value.bytes() {
+            unsafe { ptr::write_volatile(self.0, byte) }
         }
+
+        Ok(())
     }
 }
