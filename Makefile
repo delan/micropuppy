13b52039d3e0e0c63025ebec5c13d77e7b2d7eb9
the6p4c <me@doggirl.gay>
2023-10-20 21:54:23 +1000

    load kernel at a higher address to use QEMU's FDT at base of RAM

--- Makefile
+++ Makefile
@@ -6,10 +6,7 @@ CARGO_PROFILE_FLAG =
 run-kernel: kernel
 	cd qemu && make run-kernel KERNEL=../kernel/target/aarch64-unknown-none/$(CARGO_PROFILE_DIR)/kernel
 
-kernel: qemu/virt-8.0.dtb
+kernel:
 	cd kernel && cargo build $(CARGO_PROFILE_FLAG)
-
-qemu/virt-8.0.dtb:
-	cd qemu && make virt-8.0.dtb
 
 .PHONY: kernel
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
+#![deny(clippy::undocumented_unsafe_blocks)]
 
 use core::arch::global_asm;
 use core::convert::Infallible;
@@ -19,11 +20,17 @@ fn on_panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn kernel_main() {
-    // TODO not this
-    // https://qemu-project.gitlab.io/qemu/system/arm/virt.html#hardware-configuration-information-for-bare-metal-programming
-    // https://doc.coreboot.org/mainboard/emulation/qemu-aarch64.html
-    // https://mail.gnu.org/archive/html/qemu-discuss/2019-10/msg00014.html
-    let fdt = fdt::Fdt::new(include_bytes!("../../qemu/virt-8.0.dtb")).unwrap();
+    // SAFETY: QEMU loads a FDT at the base of memory (0x4000_0000) for non-Linux images (e.g. ELFs)
+    // passed to -kernel, provided that the image leaves enough space at the base of RAM for the
+    // FDT.
+    //
+    // This does mean that there may not be an FDT at this location in memory. In this case, the
+    // pointer is still valid to read from (avoiding UB) but Fdt::from_ptr will fail as the memory
+    // (hopefully) does not the FDT magic value.
+    //
+    // See https://qemu-project.gitlab.io/qemu/system/arm/virt.html#hardware-configuration-information-for-bare-metal-programming.
+    let fdt = unsafe { fdt::Fdt::from_ptr(0x4000_0000 as *const u8).unwrap() };
+
     let uart0 = fdt.find_compatible(&["arm,pl011"]).unwrap();
     let uart0 = uart0.reg().unwrap().next().unwrap();
     let mut uart0 = Pl011Writer::new(&uart0);
