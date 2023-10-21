1c986109ab0ed62bb32088d2f993c8c1bd190862
the6p4c <me@doggirl.gay>
2023-10-21 18:58:38 +1000

    remove .cargo directory, pass kernel target via cargo and linker script via build script
    we previously had a dilemma:
    - using a cargo workspace in the root of the project meant subjecting
      every crate in the workspace to the kernel's .cargo/config.toml which
      specified the custom target json. with foresight, this seemed
      inappropriate because userspace crates should be configured
      independently.
    - using a cargo workspace in kernel/ meant the kernel would require a
      Makefile which invoked a Makefile for a53 which invoked svd2rust - an
      awful lot of nesting.
    
    cargo refuses to look at a .cargo directory inside a workspace member
    when invoked in the workspace. it will, however, look at the .cargo
    directory of a workspace member if it's invoked inside the member's root
    directory. we previously relied on this to isolate the kernel's cargo
    config to the kernel itself.
    
    this, of course, broke rust-analyzer.
    
    we now use the kernel's existing build.rs, which existed to tell cargo
    to re-run when the linker script changed, to additionally pass the
    linker script to the linker. building core and specifying the target are
    achieved by passing configuration options directly to cargo.

--- Makefile
+++ Makefile
@@ -7,12 +7,13 @@ run-kernel: kernel
 	cd qemu && make run-kernel KERNEL=../target/aarch64-unknown-none/$(CARGO_PROFILE_DIR)/kernel
 
 kernel: a53
-	cd kernel && cargo build $(CARGO_PROFILE_FLAG)
+	cd kernel && make CARGO_PROFILE_FLAG=$(CARGO_PROFILE_FLAG)
 
 a53:
 	cd a53 && make
 
 clean:
+	cd kernel && make clean
 	cd a53 && make clean
 
 .PHONY: kernel a53 clean
--- kernel/build.rs
+++ kernel/build.rs
@@ -1,3 +1,20 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rerun-if-changed=src/linker.ld");
+    // Since we're in a workspace, the path we pass to the linker must be relative to the workspace,
+    // not the crate -- the linker is run in the workspace root, not in the crate. The cargo
+    // documentation states that:
+    //     In addition to environment variables, the build script’s current directory is the source
+    //     directory of the build script’s package.
+    // (see https://doc.rust-lang.org/cargo/reference/build-scripts.html#inputs-to-the-build-script)
+    // We therefore use our current directory to get a fully-qualified path to the linker script.
+    let linker_script = env::current_dir()
+        .expect("build script to have a valid current working directory")
+        .join("src/linker.ld");
+    let linker_script = linker_script
+        .to_str()
+        .expect("linker script path to be valid");
+
+    println!("cargo:rerun-if-changed={linker_script}");
+    println!("cargo:rustc-link-arg=-T{linker_script}");
 }
