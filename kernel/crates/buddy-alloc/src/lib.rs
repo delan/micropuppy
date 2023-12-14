fd059bcb78a1d98d30747c85f601e20652962b60
the6p4c <me@doggirl.gay>
2023-12-14 23:57:34 +1000

    [wip] buddy allocator

--- kernel/crates/buddy-alloc/src/lib.rs
+++ kernel/crates/buddy-alloc/src/lib.rs
@@ -16,57 +16,48 @@ pub struct Block {
 
 impl<'s> Tree<'s> {
     pub fn new(storage: &'s mut [u8], depth: usize) -> Self {
-        let sself = Self {
-            edges: storage.view_bits_mut(),
-            depth,
-        };
-
-        // a tree of depth 0 contains no nodes, and a tree of depth 1 is a boolean (and has no
-        // edges)
-        assert!(depth >= 2, "tree must be at least depth 2");
+        // a tree with depth 0 has a single node, and is just a boolean
+        assert!(depth >= 1, "tree must have depth of at least 1");
 
         // we must be able to store a complete tree's worth of edges
-        let num_nodes = (1 << depth) - 1;
-        let num_edges = num_nodes - 1;
+        let edges = storage.view_bits_mut();
+        let num_nodes = (1 << (depth + 1)) - 1;
+        let num_edges = num_nodes - 1; // every node except the root has one edge
         assert!(
-            sself.edges.len() >= num_edges,
+            edges.len() >= num_edges,
             "storage must be at least {num_edges} bits wide to store a tree of depth {depth}"
         );
 
         // initially, every edge is present (i.e. every block is unallocated)
-        sself.edges.fill(true);
+        edges.fill(true);
 
-        sself
+        Self { edges, depth }
     }
 
     pub fn allocate(&mut self, size: usize) -> Option<Block> {
-        let order = match size {
+        let height = match size {
             0 => return None,
             1 => 0,
             _ => (size - 1).ilog2() as usize + 1,
         };
-        let depth = self.depth - order - 1;
+        let depth = self.depth - height;
 
-        // find a reachable node of the correct size
-        fn find(
-            tree: &Tree,
-            node: NodeIndex,
-            predicate: &impl Fn(NodeIndex) -> bool,
-        ) -> Option<NodeIndex> {
-            if predicate(node) {
+        // find a free node at the correct depth
+        fn find(tree: &Tree, node: NodeIndex, depth: usize) -> Option<NodeIndex> {
+            if node.depth() == depth && tree.is_node_free(node) {
                 return Some(node);
             }
 
-            let (left_edge, left) = node.left_edge();
-            if tree.is_edge_present(left_edge) {
-                if let Some(left) = find(tree, left, predicate) {
+            let (left, left_edge) = node.left();
+            if tree.is_edge_free(left_edge) {
+                if let Some(left) = find(tree, left, depth) {
                     return Some(left);
                 }
             }
 
-            let (right_edge, right) = node.right_edge();
-            if tree.is_edge_present(right_edge) {
-                if let Some(right) = find(tree, right, predicate) {
+            let (right, right_edge) = node.right();
+            if tree.is_edge_free(right_edge) {
+                if let Some(right) = find(tree, right, depth) {
                     return Some(right);
                 }
             }
@@ -74,39 +65,62 @@ impl<'s> Tree<'s> {
             None
         }
 
-        let node = find(self, NodeIndex::root(), &|node| {
-            node.depth() == depth
-                && (self.is_edge_present(node.left_edge().0)
-                    && self.is_edge_present(node.right_edge().0)
-                    || order == 0)
-        })?;
+        let node = find(self, NodeIndex::root(), depth)?;
         dbg!(node, node.depth(), node.offset());
 
-        let mut blorp = Some((EdgeIndex(node.0 - 1), node));
-        while let Some((blap, blop)) = blorp {
-            self.edges.set(blap.0, false);
-            if self.is_edge_present(blap.buddy()) {
+        let mut blblb = Some(node);
+        while let Some(bbb) = blblb {
+            if let Some((parent, parent_edge)) = bbb.parent() {
+                self.edges.set(parent_edge.0, false);
+
+                if !self.is_edge_free(parent_edge.buddy()) {
+                    blblb = Some(parent);
+                } else {
+                    break;
+                }
+            } else {
                 break;
             }
-
-            blorp = blop.parent();
         }
+        self.edges.set(node.parent().unwrap().1 .0, false);
+
+        // let mut blorp = Some((EdgeIndex(node.0 - 1), node));
+        // while let Some((blap, blop)) = blorp {
+        //     self.edges.set(blap.0, false);
+        //     if self.is_edge_free(blap.buddy()) {
+        //         break;
+        //     }
+
+        //     blorp = blop.parent();
+        // }
 
         Some(Block {
-            offset: node.offset() << (self.depth - node.depth() - 1),
-            size,
+            offset: node.offset() << (self.depth - node.depth()),
+            size: 1 << height,
         })
     }
 
-    fn is_edge_present(&self, edge: EdgeIndex) -> bool {
-        let num_nodes = (1 << self.depth) - 1;
+    fn is_node_free(&self, node: NodeIndex) -> bool {
+        if node.depth() != self.depth {
+            let (_, left_edge) = node.left();
+            let (_, right_edge) = node.right();
+
+            self.is_edge_free(left_edge) && self.is_edge_free(right_edge)
+        } else {
+            let (_, parent_edge) = node.parent().unwrap();
+            self.is_edge_free(parent_edge)
+        }
+    }
+
+    fn is_edge_free(&self, edge: EdgeIndex) -> bool {
+        let num_nodes = (1 << (self.depth + 1)) - 1;
         let num_edges = num_nodes - 1;
 
         edge.0 < num_edges && self.edges[edge.0]
     }
 
     fn nodes(&self) -> impl Iterator<Item = NodeIndex> + '_ {
-        let num_nodes = (1 << self.depth) - 1;
+        let num_nodes = (1 << (self.depth + 1)) - 1;
 
         (0..num_nodes).map(NodeIndex)
     }
@@ -129,20 +143,29 @@ impl NodeIndex {
         NodeIndex(0)
     }
 
-    fn parent(self) -> Option<(EdgeIndex, NodeIndex)> {
+    fn parent(self) -> Option<(NodeIndex, EdgeIndex)> {
         if self.0 != 0 {
-            Some((EdgeIndex(self.0 - 1), NodeIndex((self.0 - 1) / 2)))
+            let parent = NodeIndex((self.0 - 1) / 2);
+            let parent_edge = EdgeIndex(self.0 - 1);
+
+            Some((parent, parent_edge))
         } else {
             None
         }
     }
 
-    fn left_edge(self) -> (EdgeIndex, NodeIndex) {
-        (EdgeIndex(2 * self.0), NodeIndex(2 * self.0 + 1))
+    fn left(self) -> (NodeIndex, EdgeIndex) {
+        let left = NodeIndex(2 * self.0 + 1);
+        let left_edge = EdgeIndex(2 * self.0);
+
+        (left, left_edge)
     }
 
-    fn right_edge(self) -> (EdgeIndex, NodeIndex) {
-        (EdgeIndex(2 * self.0 + 1), NodeIndex(2 * self.0 + 2))
+    fn right(self) -> (NodeIndex, EdgeIndex) {
+        let right = NodeIndex(2 * self.0 + 2);
+        let right_edge = EdgeIndex(2 * self.0 + 1);
+
+        (right, right_edge)
     }
 
     fn depth(self) -> usize {
@@ -170,13 +193,13 @@ impl fmt::Display for Dot<'_, '_> {
         for node in tree.nodes() {
             writeln!(f, "  n{};", node.0)?;
 
-            let (left_edge, left) = node.left_edge();
-            if tree.is_edge_present(left_edge) {
+            let (left, left_edge) = node.left();
+            if tree.is_edge_free(left_edge) {
                 writeln!(f, "  n{} -> n{};", node.0, left.0)?;
             }
 
-            let (right_edge, right) = node.right_edge();
-            if tree.is_edge_present(right_edge) {
+            let (right, right_edge) = node.right();
+            if tree.is_edge_free(right_edge) {
                 writeln!(f, "  n{} -> n{};", node.0, right.0)?;
             }
         }
@@ -193,7 +216,7 @@ mod tests {
     #[test]
     fn allocate() {
         let mut tree = [0; 2];
-        let mut tree = Tree::new(&mut tree, 4);
+        let mut tree = Tree::new(&mut tree, 3);
         //        0         size = 8
         //    0       4     size = 4
         //  0   2   4   6   size = 2
@@ -210,7 +233,7 @@ mod tests {
         assert_eq!(tree.allocate(1), Some(Block { offset: 2, size: 1 }));
         eprintln!("{}", tree.dot());
 
-        assert_eq!(tree.allocate(2), Some(Block { offset: 4, size: 2 }));
+        assert_eq!(tree.allocate(3), Some(Block { offset: 4, size: 4 }));
         eprintln!("{}", tree.dot());
 
         // assert_eq!(tree.allocate(1), Some(0));
